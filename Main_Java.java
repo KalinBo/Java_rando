67729c07ac46a6ff3a9c98c4524ccbb71c405838
kalin <kalinbobchev@gmail.com>
2025-06-17 23:16:48 +0300

    Add_Scanner_UUID

--- Main_Java.java
+++ Main_Java.java
@@ -1,13 +1,22 @@
 package Inheritance;
+import java.util.UUID;
 
 public class Main_Java extends Scann_User {
 
 	public static void main(String[] args) {
 		
 		
+		if (Scann_this().matches("yes")) {
+			System.out.println(UUID.randomUUID());
+			
+			
+			
+		}
+		else {
+		
 		System.out.println("You must use argument limited to 6 digits for the get_super_random(your number here)");
 		Random_Generator.get_super_random(200);
-		
+		}
 		
 		
 		
--- Random_Generator.java
+++ Random_Generator.java
@@ -1,13 +1,15 @@
 package Inheritance;
 
+
+
+
 public class Random_Generator extends Scann_User {
-	static int in = 345345;
-	long lo = 2374623862l;
+
 	static double ran = Math.random();
-	static double db = 34.34d;
-	static String str = "This is a double random number: ";
-	char ch = 'a';
+	
 	public static double get_super_random(double db5) {
+
+		
 		double result = ran*ran*10000/ db5;
 		if (result <= 0.01) {
 			result = result * ran * 10000;
@@ -18,11 +20,7 @@ public class Random_Generator extends Scann_User {
 		
 		System.out.print(result);
 		return result;
-		
+		}
 					
 		
-	}
-	
-	
-		
-	}
+	}	
