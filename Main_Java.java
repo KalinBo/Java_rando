f9ab17923e9b2ea1e41c47eb2674c7580610017e
kalin <kalinbobchev@gmail.com>
2025-06-17 21:41:25 +0300

    Main_random_changed

--- Main_Java.java
+++ Main_Java.java
@@ -4,7 +4,10 @@ public class Main_Java extends Scann_User {
 
 	public static void main(String[] args) {
 		Test1 ts = new Test1();
-		double db2 = Test1.get_super_random();
+		
+		System.out.println("You can use as argument digits up to 6 for get_super_random()");
+		Test1.get_super_random(4534556);
+		
 		
 		
 		
--- Test1.java
+++ Test1.java
@@ -1,19 +1,28 @@
 package Inheritance;
 
-public class Test1 {
+public class Test1 extends Scann_User {
 	static int in = 345345;
 	long lo = 2374623862l;
 	static double ran = Math.random();
 	static double db = 34.34d;
 	static String str = "This is a double random number: ";
 	char ch = 'a';
-	public static double get_super_random() {
-		double result = db+ran*db+in+in*ran;
-		System.out.println(str);
+	public static double get_super_random(double db5) {
+		double result = ran*ran*10000/ db5;
+		if (result <= 0.01) {
+			result = result * ran * 10000;
+			System.out.println(result);
+			return result;
+			
+		}
+		
 		System.out.print(result);
 		return result;
 		
+					
+		
 	}
+	
 	
 		
 	}
