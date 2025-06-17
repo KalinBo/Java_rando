90fdf22f725ae950bd5ca32a4d89a9556b9fc479
kalin <kalinbobchev@gmail.com>
2025-06-17 23:18:55 +0300

    The_Scan_file

---
+++ Scann_User.java
@@ -0,0 +1,22 @@
+package Inheritance;
+import java.util.Scanner;
+
+public class Scann_User {
+	static Scanner scanner = new Scanner(System.in);
+	
+	
+	
+	
+	
+	public static String Scann_this() {
+		
+		System.out.println("If you want to convert the random into barcode type 'yes'");
+		String user = scanner.nextLine();
+		return user;
+		
+	}
+	
+		
+
+}
+
