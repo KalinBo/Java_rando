5a505a015fa98baea888f16482e629802bf14a7c
kalin <kalinbobchev@gmail.com>
2025-06-17 22:20:02 +0300

    Changed_Class

--- Main_Java.java
+++ Main_Java.java
@@ -6,7 +6,7 @@ public class Main_Java extends Scann_User {
 		
 		
 		System.out.println("You must use argument limited to 6 digits for the get_super_random(your number here)");
-		Test1.get_super_random(200);
+		Random_Generator.get_super_random(200);
 		
 		
 		
