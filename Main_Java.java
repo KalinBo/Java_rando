adafe783f354c7a5fac8ca44e2962a7bca29caae
kalin <kalinbobchev@gmail.com>
2025-06-17 22:13:14 +0300

    Removed_unused_lines

--- Main_Java.java
+++ Main_Java.java
@@ -3,10 +3,10 @@ package Inheritance;
 public class Main_Java extends Scann_User {
 
 	public static void main(String[] args) {
-		Test1 ts = new Test1();
 		
-		System.out.println("You can use as argument digits up to 6 for get_super_random()");
-		Test1.get_super_random(4534556);
+		
+		System.out.println("You must use argument limited to 6 digits for the get_super_random(your number here)");
+		Test1.get_super_random(200);
 		
 		
 		
